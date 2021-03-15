b3df17e2d2539bdac4ef25458e1f2643b1e1aa36
Shashank Hegde <shashank.hegde@neutrinos.co>
2021-03-15 16:36:11 +0530

    removed unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SelectionComponent } from './selection/selection.component';
-import { InputComponent } from './input/input.component';
-import { MainComponent } from './main/main.component';
 import { ConverterPipe } from './selection/selection.component';
 
 @NgModule({
